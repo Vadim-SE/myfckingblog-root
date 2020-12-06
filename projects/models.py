062a8da87454051d3cd3ba146c874d9c34c4645a
Vadim <selin.vadim.internet.tariff@gmail.com>
2020-12-06 19:04:05 +0300

    Add postgreSQL DB

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+	project_image = models.ImageField(upload_to='project_images/')
+	project_text = models.CharField(max_length=300)
